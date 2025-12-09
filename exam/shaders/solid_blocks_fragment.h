bc3d2c5bd453ce591f9977cf76f9a25386f5f71a
Magnus Dybdal <magndy@stud.ntnu.no>
2025-12-09 23:10:15 +0100

    comments - Note the T-toggle flag completed in issue 10-requirement-r-3-1

--- exam/shaders/solid_blocks_fragment.h
+++ exam/shaders/solid_blocks_fragment.h
@@ -12,16 +12,18 @@ in vec3 vs_Position;        // INPUT: Position from vertex shader
 out vec4 fragColor;
 
 // Uniform
-uniform vec3 u_blockColor;
-uniform int u_textureEnabled;
+uniform vec3 u_blockColor;      // The color of the solid block based on z pos
+uniform int u_textureEnabled;   // Flag for if the textures should be shown
 
 void main()
 {
     vec4 textureColor = texture(u_CubeTextureSampler, vs_Position);
 
     if(u_textureEnabled == 0) {
+        // Flag toggled off, just use block color
         fragColor = vec4(u_blockColor, 1.0);
     } else {
+        // Texture flag on: We mix the block color with the texture
         fragColor = mix(vec4(u_blockColor, 1.0), textureColor, 0.3); 
     }
 }
